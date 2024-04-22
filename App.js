486a7829139176399e598eeb3f6315ed7eb4d5e0
Aayush Gupta <ayush385361@gmail.com>
2024-04-22 13:43:41 +0530

    Fix: cors allow url changed

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://caiai-seven.vercel.app/"); // Replace with your frontend origin
+  res.setHeader("Access-Control-Allow-Origin", "https://caiai-seven.vercel.app"); // Replace with your frontend origin
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
